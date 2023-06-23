c9ac50dd7d42342aa7359d6f21e67864fa97599f
phong nguyễn <nguyenphog1412@gmail.com>
2023-06-23 10:22:30 +0700

    Thêm confirm đơn hàng

---
+++ ProjectS/Controllers/DashSellerController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project.Data;
+using Project.Models;
+
+namespace Project.Controllers
+{
+	public class DashSellerController : Controller
+	{
+		private readonly ShopContext _shopContext;
+		public DashSellerController(ShopContext shopContext)
+		{
+			_shopContext = shopContext;
+		}
+
+
+		public IActionResult Index()
+		{
+
+            List<Bill> bills = _shopContext.Bills.Include(b => b.User).ToList();
+            return View(bills);
+		}
+
+		public IActionResult ProcessBill(int billId)
+		{
+			
+
+				var bill = _shopContext.Bills.FirstOrDefault(b => b.BillId == billId);
+
+				if (bill != null)
+				{
+					int billParse = int.Parse(bill.BillStatus) +1;
+
+
+					bill.BillStatus = $"{billParse}";
+
+					_shopContext.SaveChanges();
+				}
+
+			
+			return RedirectToAction("Index");
+		}
+	}
+}
