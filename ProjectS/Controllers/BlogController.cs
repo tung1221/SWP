6a32f8d2de9aec47c7278fb69ca53050d65ec9f7
Hatran <cauchunso2002@gmail.com>
2023-06-29 11:09:29 +0700

    css blog and blog detail

--- ProjectS/Controllers/BlogController.cs
+++ ProjectS/Controllers/BlogController.cs
@@ -1,62 +1,75 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Project.Data;
 using Project.Models;
+using System.Drawing.Imaging;
 using System.Linq;
 
 namespace Project.Controllers
 {
-    public class BlogController : Controller
-    {
-        private readonly ShopContext _context;
+	public class BlogController : Controller
+	{
+		private readonly ShopContext _context;
 
-        public BlogController(ShopContext context)
-        {
-            _context = context;
-        }
+		public BlogController(ShopContext context)
+		{
+			_context = context;
+		}
 
-        public IActionResult Index()
-        {
-            var result = from a in _context.Blogs
-                         join b in _context.ImageBlogs on a.Blogid equals b.BlogId
-                         select new { Blog = a, ImageBlog = b };
-
-            var blogs = result.Select(r => r.Blog).ToList();
-            var imageBlogs = result.Select(r => r.ImageBlog).ToList();
-
-            ViewData["Blogs"] = blogs;
-            ViewData["ImageBlogs"] = imageBlogs;
-
-            return View();
-        }
-
-        public IActionResult BlogDetails(int blogId)
-        {
-            var viewProductById = _context.Products
-                .Where(p => p.HomeStatus == true && p.BlogId == blogId)
-                .ToList();
-
-            var blog = _context.Blogs.FirstOrDefault(b => b.Blogid == blogId);
-
-            if (blog == null)
-            {
-                return NotFound();
-            }
-
-            var images = _context.ImageBlogs.Where(i => i.BlogId == blogId).ToList();
-
-            ViewData["Blog"] = blog;
-            ViewData["ImageBlogs"] = images;
-            ViewData["ViewProductById"] = viewProductById;
-
-            return View();
-        }
+		public IActionResult Index()
+		{
+			var blogs = _context.Blogs
+			.Include(b => b.ImageBlogs)
+			.Where(b => b.HomeStatus == true && b.ImageBlogs.Any(ib => ib.IsBigImg == true))
+			.Select(b => new
+			{
+				b.Blogid,
+				b.HomeStatus,
+				b.content,
+				b.DateUp,
+				FirstImage = b.ImageBlogs.Single(ib => ib.IsBigImg == true).ImageURL
+			})
+			.ToList();
 
 
+			return View(blogs);
+		}
 
+
+		public async Task<IActionResult> BlogDetails(int blogId)
+		{
+			var blog = await _context.Blogs
+				.Include(b => b.Products)
+				.Include(b => b.ImageBlogs)
+				.FirstOrDefaultAsync(b => b.Blogid == blogId);
+
+
+			if (blog == null)
+			{
+				return RedirectToAction("Index");
+			}
+
+
+			ViewData["Blog"] = blog;
+			return View();
+		}
 
 
 
 
-    }
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
 }
--- ProjectS/Models/Blog.cs
+++ ProjectS/Models/Blog.cs
@@ -1,11 +1,13 @@
 ﻿namespace Project.Models
 {
-    public class Blog
-    {
-        public int Blogid { get; set; }
-        public bool? HomeStatus { get; set; }
-        public string content { get; set; } = null!;
-        public virtual List<ImageBlog> ImageBlogs { get; set; } = null!;
-        public virtual List<Product> Products { get; set; } = null!;
-    }
+	public class Blog
+	{
+		public int Blogid { get; set; }
+		public bool? HomeStatus { get; set; }
+		public string content { get; set; } = null!;
+		public DateTime? DateUp { get; set; }
+
+		public virtual List<ImageBlog> ImageBlogs { get; set; } = null!;
+		public virtual List<Product> Products { get; set; } = null!;
+	}
 }
