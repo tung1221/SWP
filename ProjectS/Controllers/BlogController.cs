81d929ba62a5ac5520228e434743f3a7523c3932
BIOTOC <phongdb05@gmail.com>
2023-07-06 11:35:29 +0700

    v1

--- ProjectS/Controllers/BlogController.cs
+++ ProjectS/Controllers/BlogController.cs
@@ -6,16 +6,21 @@ using Project.Models;
 using System.Drawing.Imaging;
 using System.Linq;
 using System.Reflection.Metadata;
+using WebApplication6.Service;
 
 namespace Project.Controllers
 {
     public class BlogController : Controller
     {
         private readonly ShopContext _context;
+        private readonly ICloudinaryService _cloudinaryService;
 
-        public BlogController(ShopContext context)
+
+        public BlogController(ShopContext context, ICloudinaryService temp)
         {
             _context = context;
+            _cloudinaryService = temp;
+
         }
 
         public IActionResult Index()
@@ -57,5 +62,244 @@ namespace Project.Controllers
                 return View(blog);
             }
         }
+
+        public IActionResult ViewBlog()
+        {
+            LoadRoleUser();
+            List<Blog> blog = _context.Blogs.ToList();
+
+
+            return View(blog);
+        }
+
+        public IActionResult ChangeStatus(int id)
+        {
+            LoadRoleUser();
+            var blog = _context.Blogs.FirstOrDefault(b => b.Blogid == id);
+
+            if (blog != null)
+            {
+                blog.HomeStatus = !blog.HomeStatus;
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("ViewBlog");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            LoadRoleUser();
+            var blog = _context.Blogs.FirstOrDefault(b => b.Blogid == id);
+
+            if (blog != null)
+            {
+                _context.Blogs.Remove(blog);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("ViewBlog");
+        }
+
+        public IActionResult ViewDetail(int id)
+        {
+            LoadRoleUser();
+            Blog blog = _context.Blogs.Include(b => b.ImageBlogs)
+                                      .Include(b => b.Products)
+                                      .FirstOrDefault(b => b.Blogid == id);
+            return View(blog);
+        }
+
+        public IActionResult CreateBlog()
+        {
+            LoadRoleUser();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(Blog blog)
+        {
+            LoadRoleUser();
+            if (blog.content == null || blog.content2 == null || blog.name == null || blog.DateUp == null)
+            {
+                TempData["ErrorMessage"] = "Please enter all information!";
+                return Redirect($"CreateBlog?id={blog.Blogid}");
+            }
+            else
+            {
+                _context.Blogs.Add(blog);
+                _context.SaveChanges();
+
+                return Redirect($"CreateImage?id={blog.Blogid}");
+            }
+
+        }
+
+        public IActionResult CreateImage(int id)
+        {
+            LoadRoleUser();
+
+            var blog = _context.Blogs.Include(b => b.ImageBlogs).FirstOrDefault(b => b.Blogid == id);
+
+
+            return View(blog);
+        }
+
+        [HttpPost]
+        public IActionResult CreateImage(IFormFile ImageUrl, ImageBlog imageBlog)
+        {
+            var UrlImage = _cloudinaryService.UploadImage(ImageUrl, "ImageBlog");
+
+            imageBlog.ImageURL = UrlImage;
+
+            _context.ImageBlogs.Add(imageBlog);
+            _context.SaveChanges();
+
+            return Redirect($"CreateImage?id={imageBlog.BlogId}");
+
+        }
+
+        public IActionResult Update(int id)
+        {
+            LoadRoleUser();
+            var blog = _context.Blogs.FirstOrDefault(b => b.Blogid == id);
+
+            if (blog != null)
+            {
+                return View(blog);
+            }
+            else
+            {
+                return RedirectToAction("ViewBlog");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Update(Blog updatedBlog)
+        {
+            LoadRoleUser();
+            var blog = _context.Blogs.FirstOrDefault(b => b.Blogid == updatedBlog.Blogid);
+
+            if (blog != null)
+            {
+                blog.HomeStatus = updatedBlog.HomeStatus;
+                blog.content = updatedBlog.content;
+                blog.DateUp = updatedBlog.DateUp;
+                blog.name = updatedBlog.name;
+                blog.isCollection = updatedBlog.isCollection;
+                blog.content2 = updatedBlog.content2;
+
+                _context.SaveChanges();
+            }
+
+            return Redirect($"UpdateImage?id={updatedBlog.Blogid}");
+        }
+
+        public IActionResult UpdateImage(int id)
+        {
+            LoadRoleUser();
+            List<ImageBlog> img = _context.ImageBlogs
+                .Where(image => image.BlogId == id)
+                .ToList();
+
+
+
+            return View(img);
+        }
+
+        [HttpPost]
+        public IActionResult CreateImage2(IFormFile ImageUrl, ImageBlog imageBlog)
+        {
+            var UrlImage = _cloudinaryService.UploadImage(ImageUrl, "ImageBlog");
+
+            imageBlog.ImageURL = UrlImage;
+
+            _context.ImageBlogs.Add(imageBlog);
+            _context.SaveChanges();
+
+            return Redirect($"UpdateImage?id={imageBlog.BlogId}");
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateImage(int ImageId, int BlogId, string ImageUrl, bool IsBigImg, DateTime DateUp, [FromForm] IFormFile NewImageUrl, int FormId)
+        {
+            if (FormId == ImageId)
+            {
+                LoadRoleUser();
+
+                var newImageUrl = ImageUrl;
+
+                if (NewImageUrl?.Length > 0)
+                {
+                    newImageUrl = _cloudinaryService.UploadImage(NewImageUrl, "ImageBlog");
+                }
+
+                var image = _context.ImageBlogs.FirstOrDefault(i => i.ImageBlogId == ImageId);
+
+                Console.WriteLine(newImageUrl);
+
+                if (image != null)
+                {
+                    image.BlogId = BlogId;
+
+                    image.ImageURL = newImageUrl;
+
+                    image.IsBigImg = IsBigImg;
+                    image.DateUp = DateUp;
+
+                    _context.ImageBlogs.Update(image);
+                    _context.SaveChanges();
+                    return RedirectToAction("UpdateImage", new { id = BlogId });
+                }
+            }
+
+            return RedirectToAction("ViewBlog");
+        }
+
+
+
+
+
+        private void LoadRoleUser()
+        {
+            var user = HttpContext.User;
+
+            if (user.Identity.IsAuthenticated)
+            {
+                if (user.IsInRole("Admin"))
+                {
+                    ViewBag.ShowAdminButton = true;
+                }
+                else
+                {
+                    ViewBag.ShowAdminButton = false;
+                }
+
+                if (user.IsInRole("Marketing"))
+                {
+                    ViewBag.ShowMarketingButton = true;
+                }
+                else
+                {
+                    ViewBag.ShowMarketingButton = false;
+                }
+
+                if (user.IsInRole("Seller"))
+                {
+                    ViewBag.ShowSellerButton = true;
+                }
+                else
+                {
+                    ViewBag.ShowSellerButton = false;
+                }
+            }
+            else
+            {
+                ViewBag.ShowAdminButton = false;
+                ViewBag.ShowMarketingButton = false;
+                ViewBag.ShowSellerButton = false;
+            }
+        }
+
     }
 }
