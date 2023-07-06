7c1195d947282acccd77315714784815545de7ae
ntung7584 <ntung7584@gmail.com>
2023-07-06 10:27:25 +0700

    v1

--- ProjectS/Controllers/AdminController.cs
+++ ProjectS/Controllers/AdminController.cs
@@ -13,13 +13,42 @@ namespace Project.Controllers
 			_shopContext = shopContext;
             _cloudinaryService = temp;
         }
-		public IActionResult Index()
+        public IActionResult Index()
         {
-			LoadRoleUser();
-			return View();
+            LoadRoleUser();
+            DateTime now = DateTime.Now;
+            var x = _shopContext.Bills.Where(p => p.PurchaseDate.Year == now.Year).Where(p => p.BillStatus.Equals("3")).ToList();
+            double total = 0;
+            double totalMonth = 0;
+            double totalDay = 0;
+            SortedDictionary<int, double> myDictionary = new SortedDictionary<int, double>();
+
+            foreach (var l in x)
+            {
+                if (myDictionary.ContainsKey(l.PurchaseDate.Month))
+                {
+                    myDictionary[l.PurchaseDate.Month] += l.TotalPrice;
+                }
+                else
+                {
+                    myDictionary.Add(l.PurchaseDate.Month, l.TotalPrice);
+                }
+                total += l.TotalPrice;
+                if (l.PurchaseDate.Month == now.Month)
+                {
+                    totalMonth += l.TotalPrice;
+                    if (l.PurchaseDate.Day == now.Day)
+                        totalDay += l.TotalPrice;
+                }
+            }
+
+            ViewData["total"] = total;
+            ViewData["totalMonth"] = totalMonth;
+            ViewData["totaday"] = totalDay;
+            return View(myDictionary);
         }
 
-		public IActionResult cfFeedback()
+        public IActionResult cfFeedback()
 		{
 			var feedbacks = _shopContext.Feedbacks.ToList();
 
